1d64c100496ad2c2846588ae8c70821ba16cb050
ClementPan <zzz2999582@gmail.com>
2021-05-17 18:01:46 +0800

    feat: create Tech Stack section

--- src/components/TechStack.vue
+++ src/components/TechStack.vue
@@ -3,14 +3,75 @@
     <div class="title">
       <h1>Tech Stack</h1>
     </div>
+    <div class="cards">
+      <Teckcard v-for="card of cards" :key="card.id" :cardData="card" />
+    </div>
   </section>
 </template>
 
 <script>
+import frontend from "../assets/images/frontend.png";
+import backend from "../assets/images/backend.png";
+import others from "../assets/images/others.png";
+
+import Teckcard from "./subComponents/TeckCard";
 export default {
   name: "TechStack",
+  components: {
+    Teckcard,
+  },
+  data() {
+    return {
+      cards: [
+        {
+          id: 1,
+          title: "Front-end",
+          img: frontend,
+          content: [
+            "HTML / CSS / Javascript",
+            "Vue.js",
+            "Bootstrap",
+            "SCSS",
+            "RWD",
+          ],
+        },
+        {
+          id: 2,
+          title: "Backend",
+          img: backend,
+          content: [
+            "Express.js",
+            "Express-handlebars",
+            "MongoDB",
+            "Mongoose",
+            "MySQL",
+            "RESTful api",
+          ],
+        },
+        {
+          id: 3,
+          title: "others",
+          img: others,
+          content: [
+            "Git",
+            "GitHub",
+            "Postman",
+            "Npm",
+            "Heroku",
+            "Line bot",
+            "Python：web crawler",
+          ],
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
+.cards {
+  display: flex;
+  overflow-x: scroll;
+  padding: 12px 0;
+}
 </style>
---
+++ src/components/subComponents/TeckCard.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="card">
+    <div class="image">
+      <h1>{{ cardData.title }}</h1>
+      <img :src="cardData.img" alt="" />
+    </div>
+    <div class="content">
+      <p>開發經驗：</p>
+      <ul
+        class="content-list"
+        v-for="(item, index) of cardData.content"
+        :key="index"
+      >
+        <li class="list-item">
+          <img class="pointer" :src="pointer" alt="" />
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import pointer from "../../assets/images/pointer.svg";
+
+export default {
+  name: "TechCard",
+  props: {
+    cardData: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      pointer: pointer,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  min-width: 280px;
+  width: 280px;
+  height: 440px;
+  margin: 0 12px;
+  border-radius: 12px 12px 0 0;
+  overflow: hidden;
+  border: 1px solid var(--light-grey);
+}
+
+.image {
+  position: relative;
+  height: 160px;
+}
+
+.image h1 {
+  position: absolute;
+  top: 12px;
+  left: 12px;
+  color: var(--font-dark);
+  background-color: var(--font-white);
+  padding: 8px 12px;
+  border-radius: 12px;
+  font-weight: var(--title-weight);
+}
+
+.card .image img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.content {
+  padding: 16px;
+}
+
+.list-item {
+  margin: 8px 0;
+}
+
+.pointer {
+  width: 12px;
+  height: 12px;
+  transform: rotate(90deg);
+}
+</style>
